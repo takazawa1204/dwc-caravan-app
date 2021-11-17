f3e07fe432da81191b578c8983c8695b41b60bba
takazawa1204 <ckicken1204@gmail.com>
2021-11-17 02:03:48 +0000

    6章

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def index
@@ -13,12 +14,20 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blogs_path(blog.id)
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
-
+  
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+    
+    
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
